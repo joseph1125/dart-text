c8b83e96c558be8d7e1e78f4e900228f2f51a2cf
Joseph Chong <joseph.chong@neard.com>
2021-02-10 19:03:41 +0800

    TODO: substring in dart

--- lib/src/functions.dart
+++ lib/src/functions.dart
@@ -3,3 +3,9 @@ import 'package:characters/characters.dart';
 int getTextLength(String text) {
   return text.characters.length;
 }
+
+String limitText(String text, int count) {
+  return text.characters.take(count).toString();
+}
+
+String substring(String text, int startIndex, int endIndex) {}
