c2da245f124989a25179c769e239f3a53a52be8b
Joseph Chong <joseph.chong@neard.com>
2021-03-04 16:01:35 +0800

    updated dart characters package

--- lib/dart_text.dart
+++ lib/dart_text.dart
@@ -7,10 +7,7 @@ int length(String text) {
 }
 
 String substring(String text, int startIndex, [int endIndex]) {
-  return text.characters
-      .take(max(endIndex ?? length(text), 0))
-      .skip(max(startIndex, 0))
-      .toString();
+  return text.characters.getRange(startIndex, endIndex).toString();
 }
 
 String substr(String text, int startIndex, [int count]) {
